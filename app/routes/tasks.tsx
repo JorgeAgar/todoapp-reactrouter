a6afe8c5b6c4cbd8e56f0f59fb2b17418fdd098a
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2026-02-05 15:12:24 -0500

    Added button to header in tasks

--- app/routes/tasks.tsx
+++ app/routes/tasks.tsx
@@ -1,4 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 export default function Tasks() {
   return (
@@ -12,6 +13,22 @@ export default function Tasks() {
 function Header() {
   return (
     <header className="w-full flex flex-row justify-between items-center bg-black p-2">
+      <Button variant="ghost" className="text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+          />
+        </svg>
+      </Button>
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" />
         <AvatarFallback>CN</AvatarFallback>
