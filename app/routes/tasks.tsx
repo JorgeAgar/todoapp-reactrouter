3cfe3df1e0389303ff79e64e58f8a9f96ba5c1d5
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2026-02-05 10:49:29 -0500

    added avatar component to header

--- app/routes/tasks.tsx
+++ app/routes/tasks.tsx
@@ -1,3 +1,21 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function Tasks() {
-    return <h1>Tasks Page</h1>;
-};
+  return (
+    <div>
+      <Header />
+      <main>tasks</main>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="w-full flex flex-row justify-between items-center bg-black p-2">
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </header>
+  );
+}
