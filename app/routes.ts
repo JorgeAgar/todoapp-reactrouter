4b17259ec4a01774e39838aafb4bd31b8f89abe9
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2026-02-09 16:25:34 -0500

    Added signup UI

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,13 @@
-import { type RouteConfig, route, index } from "@react-router/dev/routes";
+import { type RouteConfig, route, index, prefix } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   route("tasks", "routes/tasks.tsx"),
+  route("signup", "routes/signup.tsx"),
+  route("login", "routes/login.tsx"),
+  ...prefix("api", [
+    ...prefix("auth", [
+      route("*", "routes/auth-catcher.ts"),
+    ])
+  ])
 ] satisfies RouteConfig;
